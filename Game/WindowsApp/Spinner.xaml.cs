8ef629e00ca37c5c1ab83d99da8e2054b0848dc8
Baharaz <35997893+Baharaz@users.noreply.github.com>
2018-02-21 11:38:21 +0100

    spinner toegevoegd
    Spinner toegevoegd

--- Game/WindowsApp/Spinner.xaml.cs
+++ Game/WindowsApp/Spinner.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.Toolkit.Uwp;
+using Microsoft.Toolkit.Uwp.UI.Animations;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,36 +15,20 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Spinner : Page
     {
-        public MainPage()
+        public Spinner()
         {
             this.InitializeComponent();
-            
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ((Button)sender).Content = "Hallo";
-            
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            
-
+            var animation = SpinnerButton.Rotate(value: 3600f, centerX: 150, centerY: 150, duration: 10000, delay: 200, easingType: EasingType.Circle);
+            animation.Start();
         }
     }
 }
--- Game/WindowsApp/VraagPagina.xaml.cs
+++ Game/WindowsApp/VraagPagina.xaml.cs
@@ -13,36 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class VraagPagina : Page
     {
-        public MainPage()
+        public VraagPagina()
         {
             this.InitializeComponent();
-            
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ((Button)sender).Content = "Hallo";
-            
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            
-
         }
     }
 }
